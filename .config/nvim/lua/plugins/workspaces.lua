1d10e1a05fa9adf9756d54621c17600622d92a2d
sasank-desaraju <sasank.desaraju@ufl.edu>
2024-06-30 11:12:07 -0400

    Didn't do too much with workspaces. Mostly just had the code autoformatted lol.

--- .config/nvim/lua/plugins/workspaces.lua
+++ .config/nvim/lua/plugins/workspaces.lua
@@ -1,34 +1,35 @@
 return {
 
   { -- manage projects
-    'gnikdroy/projections.nvim',
+    -- TODO: Can this be useful to me? I don't know quite what it does
+    "gnikdroy/projections.nvim",
     keys = {
       {
-        '<leader>fp',
+        "<leader>fp",
         function()
-          vim.cmd 'Telescope projections'
+          vim.cmd("Telescope projections")
         end,
-        desc = '[p]rojects',
+        desc = "[p]rojects",
       },
     },
     config = function()
       -- Save localoptions to session file
-      vim.opt.sessionoptions:append 'localoptions'
-      require('projections').setup {
+      vim.opt.sessionoptions:append("localoptions")
+      require("projections").setup({
         store_hooks = {
           pre = function()
             -- nvim-tree
-            local nvim_tree_present, api = pcall(require, 'nvim-tree.api')
+            local nvim_tree_present, api = pcall(require, "nvim-tree.api")
             if nvim_tree_present then
               api.tree.close()
             end
           end,
         },
-      }
+      })
 
       -- Autostore session on VimExit
-      local Session = require 'projections.session'
-      vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
+      local Session = require("projections.session")
+      vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
         callback = function()
           local cwd = vim.loop.cwd()
           if cwd ~= nil then
@@ -38,8 +39,8 @@ return {
       })
 
       -- Switch to project if vim was started in a project dir
-      local switcher = require 'projections.switcher'
-      vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+      local switcher = require("projections.switcher")
+      vim.api.nvim_create_autocmd({ "VimEnter" }, {
         callback = function()
           if vim.fn.argc() == 0 then
             local cwd = vim.loop.cwd()
